14933957d7d193ac2b6e2cbf1417ae1184e334a8
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-03-30 21:50:12 +0200

    chore: cleanup

--- chat/components/pydantic_form/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Pydantic form component for automatic form generation from Pydantic models."""
-
-from __future__ import annotations
-
-from .base import FieldHandler, FieldHandlerRegistry, pydantic_form
-
-__all__ = ["FieldHandler", "FieldHandlerRegistry", "pydantic_form"]
